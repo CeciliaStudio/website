06cd3f8109f0a25dbf46a02fefa0fcff7a17086d
AnemoFlower <yizhimcqiu@icloud.com>
2025-12-24 11:37:20 +0800

    feat: 文章系统

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,16 @@
-import DefaultTheme from "vitepress/theme";
-import type { App } from "vue";
+import type { Theme } from "vitepress"
+import DefaultTheme from "vitepress/theme"
+import type { App } from "vue"
 
-import './custom.css'
+import "./custom.css"
 
-import Article from "./layouts/Article.vue";
+import Article from "./layouts/Article.vue"
+import ArticleList from "./components/ArticleList.vue"
 
 export default {
     ...DefaultTheme,
     enhanceApp({ app }: { app: App }) {
         app.component("article", Article)
+        app.component("ArticleList", ArticleList)
     }
-}
+} satisfies Theme
